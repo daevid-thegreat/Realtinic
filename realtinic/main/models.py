9933a37c27ae5b6cc3659acfb3e2f6f320155b94
David Makinde <mckinz147@gmail.com>
2022-03-28 10:52:33 +0100

    jnibygyggih

--- realtinic/main/models.py
+++ realtinic/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Userprofile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    location = models.CharField(max_length=120, blank=True)
+
+
+    def __str__(self):
+        return self.user.full_name
--- realtinic/main/views.py
+++ realtinic/main/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Userprofile
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(email=email).exists():
+            messages.info(request, 'Email is taken')
+            return redirect('index')
+        else:
+            user = User.objects.create_user(full_name=full_name, email=email, password=password)
+            user.save() 
+
+            user_model = User.objects.get(full_name=full_name)
+            new_profile = Userprofile.objects.create(user=user_model, id_user=user_model.id)
+            new_profile.save()
+            return redirect('index.html')
+    else:
+        return render(request, 'index.html')
